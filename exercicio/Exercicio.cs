19d90a4459e5790cfb9c01c4fe3d8197a6f1a244
milena <milena-.dias@live.com>
2024-06-10 12:55:48 -0300

    If Else

---
+++ exercicio/Exercicio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+
+class Exercicio 
+{
+    public static void Main(string[] args) 
+    {
+        //forma braço curto de if else, no caso "?" é if, ":" é else
+        int time = 20;
+        string result = (time < 18) ? "Good day." : "Good Evening.";
+        Console.WriteLine(result);
+    }
+}
